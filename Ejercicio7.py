a1b06f76e8909ccfddb0814e51914f54e56b9c2e
tudor204 <tudor204@hotmail.com>
2025-05-24 08:50:28 +0200

    Add files via upload

---
+++ Ejercicio7.py
@@ -0,0 +1,11 @@
+"""
+Escribir un programa que almacene el abecedario en una lista, 
+elimine de la lista las letras que ocupen posiciones múltiplos de 3,
+ y muestre por pantalla la lista resultante.
+"""
+
+diccionario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(len(diccionario),1,-1):
+    if i % 3 == 0:
+        diccionario.pop(i-1)
+print(diccionario)
